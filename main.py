74931e9cf92d887f755666c5be9eb264d97c6747
Дмитрий <dmitrijadarov@gmail.com>
2025-02-13 01:28:23 +0300

    add support for multiple systems

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import os
+import platform
 import shutil
 import subprocess
-dir = os.getenv('HOME') + "/.config/QtProject"
-with open("forConfig.ini", "r", encoding="utf-8") as src:
-    code_to_add = src.read()
-print(code_to_add)
-with open(dir+"/QtCreator.ini", "a", encoding="utf-8") as dest:
-    dest.write("\n"+code_to_add)
-source_folder = "beautifier"
-destination_folder = dir+"/qtcreator"
-shutil.copytree(source_folder, os.path.join(destination_folder, os.path.basename(source_folder)), dirs_exist_ok=True)
-source_folder = "clang-format"
-shutil.copytree(source_folder, os.path.join(destination_folder, os.path.basename(source_folder)), dirs_exist_ok=True)
-subprocess.run("sudo apt install clazy && sudo apt install clang-format", shell=True)
+if platform.system == "Linux":
+    dir = os.getenv('HOME') + "/.config/QtProject"
+    with open("forConfig.ini", "r", encoding="utf-8") as src:
+        code_to_add = src.read()
+    with open(dir+"/QtCreator.ini", "a", encoding="utf-8") as dest:
+        dest.write("\n"+code_to_add)
+    source_folder = "beautifier"
+    destination_folder = dir+"/qtcreator"
+    shutil.copytree(source_folder, os.path.join(destination_folder, os.path.basename(source_folder)), dirs_exist_ok=True)
+    source_folder = "clang-format"
+    shutil.copytree(source_folder, os.path.join(destination_folder, os.path.basename(source_folder)), dirs_exist_ok=True)
+    subprocess.run("sudo apt install clazy && sudo apt install clang-format", shell=True)
+elif platform.system == "Windows":
