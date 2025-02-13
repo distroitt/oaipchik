6f31d7cfb02693e0276be83b82cc59ae46eee27e
distroitt <dmitrijadarov@gmail.com>
2025-02-13 21:21:40 +0300

    modify script file

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import platform
 import shutil
 import subprocess
-if platform.system == "Linux":
+if platform.system() == "Linux":
     dir = os.getenv('HOME') + "/.config/QtProject"
     with open("forConfigUbuntu.ini", "r", encoding="utf-8") as src:
         code_to_add = src.read()
@@ -13,8 +13,8 @@ if platform.system == "Linux":
     shutil.copytree(source_folder, os.path.join(destination_folder, os.path.basename(source_folder)), dirs_exist_ok=True)
     source_folder = "clang-format"
     shutil.copytree(source_folder, os.path.join(destination_folder, os.path.basename(source_folder)), dirs_exist_ok=True)
-    subprocess.run("sudo apt update && sudo apt upgrade -y && sudo apt install -y clazy && sudo apt install -y clang-format && sudo apt-get install -f", shell=True)
-elif platform.system == "Darwin":
+    subprocess.run("sudo apt update && sudo apt upgrade -y && sudo apt install -y clazy && sudo apt install -y clang-format && sudo apt install -y cmake && sudo apt-get install -f", shell=True)
+elif platform.system() == "Darwin":
     dir = os.getenv('HOME') + "/.config/QtProject"
     with open("forConfigMac.ini", "r", encoding="utf-8") as src:
         code_to_add = src.read()
