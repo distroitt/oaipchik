7144d6d44595363ea8f13fefcf9bccee8321ef1a
Дмитрий <dmitrijadarov@gmail.com>
2025-02-13 09:15:43 +0300

    add macos support

--- main.py
+++ main.py
@@ -14,4 +14,17 @@ if platform.system == "Linux":
     source_folder = "clang-format"
     shutil.copytree(source_folder, os.path.join(destination_folder, os.path.basename(source_folder)), dirs_exist_ok=True)
     subprocess.run("sudo apt install clazy && sudo apt install clang-format", shell=True)
-elif platform.system == "Windows":
+elif platform.system == "Darwin":
+    dir = os.getenv('HOME') + "/.config/QtProject"
+    with open("forConfig.ini", "r", encoding="utf-8") as src:
+        code_to_add = src.read()
+    with open(dir+"/QtCreator.ini", "a", encoding="utf-8") as dest:
+        dest.write("\n"+code_to_add)
+    source_folder = "beautifier"
+    destination_folder = dir+"/qtcreator"
+    shutil.copytree(source_folder, os.path.join(destination_folder, os.path.basename(source_folder)), dirs_exist_ok=True)
+    source_folder = "clang-format"
+    shutil.copytree(source_folder, os.path.join(destination_folder, os.path.basename(source_folder)), dirs_exist_ok=True)
+    subprocess.run("sudo brew install clazy && sudo brew install clang-format", shell=True)
+else:
+    
