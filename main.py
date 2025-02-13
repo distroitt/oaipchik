f9e3eb4a90226c04c018bb0ded4a4d79751325c9
Дмитрий <dmitrijadarov@gmail.com>
2025-02-14 01:00:06 +0300

    add validation

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import platform
 import shutil
 import subprocess
 if platform.system() == "Linux":
+    print("Введите фамилию: )
+    surname = input()
     dir = os.getenv('HOME') + "/.config/QtProject"
     with open("forConfigUbuntu.ini", "r", encoding="utf-8") as src:
         code_to_add = src.read()
@@ -14,6 +16,15 @@ if platform.system() == "Linux":
     source_folder = "clang-format"
     shutil.copytree(source_folder, os.path.join(destination_folder, os.path.basename(source_folder)), dirs_exist_ok=True)
     subprocess.run("sudo apt update && sudo apt upgrade -y && sudo apt install -y clazy && sudo apt install -y clang-format && sudo apt install -y cmake && sudo apt-get install -f", shell=True)
+    print("Для проверки установки проделайте шаги указанные в файле check.txt, если все успешно, нажмите Y, иначе N")
+    while True:
+        user_input = input().strip().upper()
+        if user_input == 'Y':
+            pass
+        elif user_input == 'N':
+            pass
+        else:
+        print("Неверный ввод! Пожалуйста, нажмите Y или N.")
 elif platform.system() == "Darwin":
     dir = os.getenv('HOME') + "/.config/QtProject"
     with open("forConfigMac.ini", "r", encoding="utf-8") as src:
